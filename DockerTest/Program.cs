685081397f741960076a364097a0d4380a996c02
MarcinGała <marcingala81@gimail.com>
2023-08-07 20:35:26 +0200

    Second Commit

--- DockerTest/Program.cs
+++ DockerTest/Program.cs
@@ -12,11 +12,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
 }
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
